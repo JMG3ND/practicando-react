a19565c28397ff7a9e4fb88cc3b7498276b34096
Jose Gutierrez <jo0303se@hotmail.com>
2024-08-27 19:03:04 -0500

    juego-funcional

--- 02-cruz-y-cero/src/Board.jsx
+++ 02-cruz-y-cero/src/Board.jsx
@@ -8,23 +8,25 @@ export default function Board() {
   const [squares, setSquares] = useState(Array(9).fill(null));
   const [turn, setTurn] = useState(turns.X);
   const [showModal, setShowModal] = useState(false);
+  const [wining, setWining] = useState(false);
 
   const handleClick = (index) => {
-    if (squares[index] != null) return;
+    if (squares[index] != null || checkWin(squares)) return;
 
     const newSquare = squares.slice();
     newSquare[index] = turn;
     setSquares(newSquare);
     if (checkWin(newSquare)) {
+      setWining(true);
       setShowModal(true);
       return;
     }
     setTurn(turn === turns.X ? turns.O : turns.X);
   };
-  const okClick = () => {
+  const restart = () => {
     setSquares(Array(9).fill(null));
     setTurn(turns.X);
-    setShowModal(false);
+    setWining(false);
   };
 
   return (
@@ -44,8 +46,17 @@ export default function Board() {
         <Square value={squares[7]} onSquareClick={() => handleClick(7)} />
         <Square value={squares[8]} onSquareClick={() => handleClick(8)} />
       </div>
-      <h2 className="board-turn">Turno: {turn}</h2>
-      {showModal && <Modal winer={turn} onOkClick={okClick} />}
+      <h2 className="board-turn">
+        {wining ? "Ganador: " : "Turno: "} {turn}
+      </h2>
+      <div className="board-turn">
+        <button onClick={restart} className="modal__button_ok">
+          Reiniciar
+        </button>
+      </div>
+      {showModal && (
+        <Modal winer={turn} onOkClick={() => setShowModal(false)} />
+      )}
     </>
   );
 }
